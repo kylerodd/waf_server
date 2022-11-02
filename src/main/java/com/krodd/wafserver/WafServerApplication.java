0e0693613d1bc5401a267398abe752d08c1e2592
kgrodd2243 <kyleglennrodd@gmail.com>
2022-11-01 20:22:25 -0500

    only initializr -> don't run

---
+++ src/main/java/com/krodd/wafserver/WafServerApplication.java
@@ -0,0 +1,13 @@
+package com.krodd.wafserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WafServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WafServerApplication.class, args);
+	}
+
+}
